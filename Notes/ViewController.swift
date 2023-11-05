53a0bd729628efaac483df8ef11e46e3eaa0071f
TeklaMach <macharashvilitekla@gmail.com>
2023-11-05 22:54:43 +0400

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Tekla Matcharashvili on 05.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
